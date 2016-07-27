8c325f268116e85116248806ab58c7525e753ec7
Domenico <d-caruso@hotmail.it>
2016-07-27 14:36:05 +0200

    Add files via upload

--- apache_dir.py
+++ apache_dir.py
@@ -62,6 +62,12 @@ def what_is(line):
     if re.match('</Directory>', line):
        return 'endDirectory'
 
+    if re.match('<VirtualHost ', line): # notice the space
+       return 'VirtualHost'
+    if re.match('</VirtualHost>', line):
+       return 'endVirtualHost'
+
+
     if re.match('<[a-z-A-Z]',line):
        return 'section'
     if re.match('</',line):
@@ -152,6 +158,11 @@ def parse_apache(old, new):
            print >> new, line.strip("\n")
         if result == 'endDirectory':
            print >> new, line.strip("\n")
+
+        if result == 'VirtualHost':
+           print >> new, line.strip("\n")
+        if result == 'endVirtualHost':
+           print >> new, line.strip("\n")
    
         if result == 'IfModule':
            check_IFmodule(line, old, new)
@@ -163,14 +174,21 @@ def parse_apache(old, new):
         if result == 'endIfDefine':
            return
 
+################### Creating objects and setting parents  ############
+
+# we need an object that can hold the information of each <Direcotry>
+# so, each time we encounted a <Directory> we crate an object and each Directives will be added as object attribute
 class Directory(object):
    'Common base class for all directory sections'
 
    def __init__(self, key):
       self.name = key
-   
-def create_dir(line,f,name):
+
+
+# creation of the object and attributes
+def create_object(line,f,name,TAG):
     obj = Directory(name)
+    setattr(obj,'TAG', TAG)
     for line in f:
         result = what_is(line)
         if result == 'endDirectory':
@@ -180,51 +198,182 @@ def create_dir(line,f,name):
            value = line.split(key)[1]
            setattr(obj,key, value)
 
-def find_dir():
-    f = open ('parsed')
-    global all_dirs
-    all_dirs = []
-    global all_obj
-    all_obj = []
+# in case a direcotry has been defined multiple times, we just add the directives to the obj
+def append_to_obj(f,obj):
     for line in f:
         result = what_is(line)
+        if result == 'endDirectory':
+           return obj
+        else:
+           key = line.split()[0]
+           value = line.split(key)[1]
+           setattr(obj,key, value)
+
+# this is the main function that read the file and when finds <Directory> it call the create function
+def find_dir():
+    f = open ('parsed')
+    TAG = 'general'  # if the directory is located in the global keys
+    for line in f:
+        result = what_is(line)
+        if result == 'VirtualHost':
+           TAG = line.lstrip().split()[1].replace(">","")   # in case the directory is nested in a VirtualHost
         if result == 'Directory':
            try:
                name = line.lstrip().split()[1].replace('>','').strip("\"")
            except:
                name = line.lstrip().split()[1].replace('>','')
-           all_dirs.append(name)
-           all_obj.append(create_dir(line,f,name))
+           if name in all_dirs:
+              for obj in all_obj:
+                  if obj.name == name:
+                     append_to_obj(f,obj)
+           else:
+              all_dirs.append(name)
+              all_obj.append(create_object(line,f,name,TAG))
+        if result == 'endVirtualHost':
+           TAG = 'general'
+
     all_dirs.sort(reverse=True)
     all_obj.sort(reverse=True)
     set_parent()
-    print_parent()
 
 def check_parenthood(child,parent):
+    for each in all_obj:
+        if child == each.name:
+           obj_child = each
+        if parent == each.name:
+           obj_parent = each
+
     if parent in child:
        if parent == child:
-          return
+          return 'notparent'
        else:
-          return 'parent'
+          if obj_parent.TAG == 'general' or obj_parent.TAG == obj_child.TAG:
+             return 'parent'
+          else:
+             return 'notparent'
 
+# once we have all object we should set a parent for each of them
+# so that for each <Directory> we can inherit what has been already set for the parent
 def set_parent():
     for child in all_dirs:
         for parent in all_dirs:
            if check_parenthood(child,parent) == 'parent':
-              for each in all_obj: 
+              for each in all_obj:
                   if each.name == child:
-                     #print 'Setting parent ' +parent +' for child ' +each.name
                      setattr(each,'parent',parent)
               break
 
-def print_parent():
-    for each in all_obj:
+
+
+######### Merging ######################
+def check_directives():
+    all_dirs.sort()
+    for each in all_dirs:
+        for obj in all_obj:
+            if obj.name == each:
+#              print "Prima: ", vars(obj)
+              options = check_options(obj)
+              check_other_directives(obj)
+#              print "Dopo: ", vars(obj)
+              break
+
+def merge_options(child_options,parent_options):
+    final = []
+    for each in child_options:
+        if re.match('\+',each):
+            final.append(each)
+    for each in parent_options:
+        if '-'+each in child_options:
+           continue
+        if '+'+each in child_options:
+           continue
+        else:
+           final.append(each)
+    return ' '.join(final)
+
+def find_parent(child):
+    for parent in all_obj:
+        if parent.name == child.parent:
+           return parent
+
+def check_options(child):
+    if hasattr(child,'parent'):
+       parent = find_parent(child)
+       parent_options = parent.Options.split()
+       try:
+          child_options = child.Options.split()
+       except:
+          final = ' '.join(parent_options)
+          setattr(child,'Options',final)
+          child_options = child.Options.split()
+       for each in child_options:
+           if re.match('[+-]',each):
+              final = merge_options(child_options,parent_options)
+              setattr(child,'Options',final)
+              child_options = child.Options.split()
+           else:
+              return child_options
+    else:
         try:
-           print each.name +' has as parent ' +each.parent
+          return child.Options.split()
         except:
-           print each.name +' has no parent'
+          setattr(child,'Options',' ')
+          return child.Options.split()
 
 
+def check_other_directives(child):
+    if hasattr(child,'parent'):
+       dict_directives_child = vars(child)
+       parent = find_parent(child)
+       dict_directives_parent = vars(parent)
+       for directive in dict_directives_parent:
+           if directive not in dict_directives_child:
+              attribute = getattr(parent,directive)
+              setattr(child,directive,attribute)
+
+
+################# Print final configuration #####################    
+
+def jump_directory(f):
+    for line in f:
+        result = what_is(line)
+        if result == 'endDirectory':
+           return
+        else:
+           continue
+
+def write_new_directory(f1,line):
+    print >> f1, line.strip("\n")
+    try:
+       name = line.lstrip().split()[1].replace('>','').strip("\"")
+    except:
+       name = line.lstrip().split()[1].replace('>','')
+    for each in all_obj:
+       if each.name == name:
+          for uno in vars(each):
+              if uno == 'parent' or uno == 'name' or uno == 'TAG':
+                 continue
+              print >> f1, uno, vars(each)[uno].strip("\n")
+    return
+
+def print_final():
+    f = open('parsed')
+    f1 = open('parsed_finale','aw')
+    for line in f:
+        result = what_is(line)
+        if result == 'Directory':
+           jump_directory(f)
+           write_new_directory(f1,line)
+           print >> f1, '</Directory>'
+           
+           
+        else:
+           print >> f1, line.strip("\n")
+    
+
+
+
+############# Main ###################
 if __name__ == "__main__":
     # the starting point is the main apache config file
     global apache_dir
@@ -239,6 +388,7 @@ if __name__ == "__main__":
     global all_defines
     all_defines = []
     
+########## Parsing  ##################
     # this function will start reading the main config file and print each line; when it finds an Include, it will read in aplphabetical order each file and then return to the main;
     # when it finds LoadModule or Define, it will store these into the global vars all_modules and all_defines;
     # the IfModule and IfDefine are evaluated as they are found because Apache evaluates them at startup; in case condition is not met, the all section will be jumped, else, printed
@@ -247,4 +397,18 @@ if __name__ == "__main__":
     parsed.close()
 
 
+##### Creating objects and setting parents ########
+    global all_dirs
+    all_dirs = []
+    global all_obj
+    all_obj = []
     find_dir() 
+
+
+########## Merging ###############
+
+    check_directives()
+
+######### Print final config ###############
+    
+    print_final()
